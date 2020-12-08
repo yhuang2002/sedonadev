a93832adb63e85c124ad7641749855ab37b4e693
Ying Huang <yhuang2002@gmail.com>
2020-12-08 20:20:49 +0000

    Rename ESPPub to Pub and add Sub

--- src/ESPPubSub/native/espPubSub.c
+++ src/ESPPubSub/native/espPubSub.c
@@ -10,7 +10,7 @@
 /*****************************************************************************/
 
 // int doEspPublishMsg(int Pub_ID, float value)
-Cell ESPPubSub_EspPubSub_doEspPublishMsg(SedonaVM* vm, Cell* params) {
+Cell ESPPubSub_PubSub_doPublishMsg(SedonaVM* vm, Cell* params) {
   int pubId     = params[0].ival;
   float value   = params[1].fval;
   Cell val;
@@ -22,7 +22,7 @@ printf("EspPub publish value %f\n", value);
 }
 
 // int doEspPublishInit(Str topic)
-Cell ESPPubSub_EspPubSub_doEspPublishInit(SedonaVM* vm, Cell* params) {
+Cell ESPPubSub_PubSub_doPublishInit(SedonaVM* vm, Cell* params) {
   char *topic = params[0].aval;
   Cell val;
   val.ival = 3;
@@ -33,7 +33,7 @@ printf("EspPub publish setup for %s\n", topic);
 }
 
 // int doEspPublishDeinit(Str topic)
-Cell ESPPubSub_EspPubSub_doEspPublishDeinit(SedonaVM* vm, Cell* params) {
+Cell ESPPubSub_PubSub_doPublishDeinit(SedonaVM* vm, Cell* params) {
   char pubId = params[0].ival;
   Cell val;
   val.ival = 0;
@@ -44,7 +44,7 @@ printf("EspPub publish setup remove id = %d\n", pubId);
 }
 
 // int doEspSubscrib(Str topic)
-Cell ESPPubSub_EspPubSub_doEspSubscrib(SedonaVM* vm, Cell* params)
+Cell ESPPubSub_PubSub_doSubscribe(SedonaVM* vm, Cell* params)
 {
   char *topic = params[0].aval;
   Cell val;
@@ -54,7 +54,7 @@ Cell ESPPubSub_EspPubSub_doEspSubscrib(SedonaVM* vm, Cell* params)
 }
 
 // int doEspUnsubscrib(int Sub_ID)
-Cell ESPPubSub_EspPubSub_doEspUnsubscrib(SedonaVM* vm, Cell* params)
+Cell ESPPubSub_PubSub_doUnsubscribe(SedonaVM* vm, Cell* params)
 {
   int id = params[0].ival;
 
@@ -62,7 +62,7 @@ Cell ESPPubSub_EspPubSub_doEspUnsubscrib(SedonaVM* vm, Cell* params)
 }
 
 // float doEspSubscribRead(int Sub_ID)
-Cell ESPPubSub_EspPubSub_doEspSubscribRead(SedonaVM* vm, Cell* params)
+Cell ESPPubSub_PubSub_doSubscribeRead(SedonaVM* vm, Cell* params)
 {
   int id = params[0].ival;
 
